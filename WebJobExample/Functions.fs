b22452b6e154b64acab891eb5924fefaeeef9e30
Ryan Riley <ryanriley@live.com>
2017-12-29 13:31:23 -0600

    Update example

--- WebJobExample/Functions.fs
+++ WebJobExample/Functions.fs
@@ -1,14 +1,24 @@
 module WebJobExample.Functions
 
+open System
 open System.Configuration
 open System.IO
+open System.Threading.Tasks
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Host
 
-let processQueueMessage ([<QueueTrigger("%InputQueue%"); StorageAccount("%StorageAccount%")>] message : string) (log : TextWriter) =
+let processQueueMessage ([<QueueTrigger("%InputQueue%"); StorageAccount("%StorageAccount%")>]message:string, log:TextWriter) =
     message |> log.WriteLine
 
 [<Disable("TimerDisable")>]
-let run ([<TimerTrigger("%TimerTrigger%")>] timer, log: TraceWriter) =
-    let setting = ConfigurationManager.AppSettings.["InputQueue"]
-    log.Info(sprintf "The queue processor is listening to %s" setting)
+let run ([<TimerTrigger("%TimerTrigger%")>]timer:TimerInfo, log:TraceWriter) =
+    // Async is unnecessary here but used to show how to correctly return the Task.
+    async {
+        log.Info(
+            sprintf "F# Timer trigger function executed at: %s" 
+                (DateTime.Now.ToString()))
+        log.Info(
+            sprintf "The queue processor is listening to %s"
+                (ConfigurationManager.AppSettings.["InputQueue"]))
+    }
+    |> Async.StartAsTask :> Task
