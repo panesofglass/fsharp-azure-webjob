baf451ee367981d409c1d0b781ab90cdb0ad1ac1
Ryan Riley <ryan@tachyus.com>
2018-12-18 23:14:02 -0600

    Try adding configNameResolver

--- WebJobExample/Program.fs
+++ WebJobExample/Program.fs
@@ -1,6 +1,17 @@
 module WebJobExample.Program
 
+open System
+open System.Configuration
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+let configNameResolver =
+    { new INameResolver with
+        member __.Resolve(name) =
+            ConfigurationManager.AppSettings.[name] }
 
 [<EntryPoint>]
 let main argv =
@@ -11,6 +22,21 @@ let main argv =
                 b.AddAzureStorageCoreServices()
                  .AddAzureStorage()
                  .AddTimers() |> ignore
+            )
+            .ConfigureLogging(fun b ->
+                b.SetMinimumLevel(LogLevel.Debug) |> ignore
+
+                let appInsightsKey = ""
+                if not (String.IsNullOrEmpty appInsightsKey) then
+                    b.AddApplicationInsights(fun config -> config.InstrumentationKey <- appInsightsKey) |> ignore
+            )
+            .ConfigureServices(fun services ->
+                // This seems to correctly add the name resolver for the QueueTrigger
+                services.AddSingleton(configNameResolver) |> ignore
+                // Next error is that the ExampleStorage connection string cannot be found
+                //services.AddSingleton(
+                //    StorageAccountProvider( ???
+                //            ConfigurationManager.ConnectionStrings.["ExampleStorage"].ConnectionString)) |> ignore
             )
             .UseConsoleLifetime()
 
